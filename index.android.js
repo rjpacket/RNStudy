39d5adda6f2f0a1944e3c678b1e15e9adbd62802
rjpacket <jimbo922@163.com>
2018-03-23 17:41:14 +0800

    自定义组件

--- index.android.js
+++ index.android.js
@@ -6,15 +6,29 @@
    Text,
    View
  } from 'react-native';
+ class Btn extends React.Component{
+    render() {
+        return (
+            <Text style={styles.hello}>Hello {this.props.name}</Text>
+        );
+    }
+ }
+
  class HelloWorld extends React.Component {
    render() {
      return (
        <View style={styles.container}>
          <Text style={styles.hello}>Hi!第一个项目诞生了！</Text>
+         <Btn name="rjp1"></Btn>
+         <Btn name="rjp2"></Btn>
+         <Btn name="rjp3"></Btn>
+         <Btn name="rjp2"></Btn>
+         <Btn name="rjp1"></Btn>
        </View>
      )
    }
  }
+
  var styles = StyleSheet.create({
    container: {
      flex: 1,
